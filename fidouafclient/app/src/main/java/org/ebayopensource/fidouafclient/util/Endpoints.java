6f2d1984b6d40afcab3d3f1b11c1c8f59e6ff257
Andrés Pescara <andrespescara@hotmail.com>
2017-06-21 20:43:42 -0400

    cambio SERVER endpoint
    se modifico el archivo Endpoints en la carpeta Utils para que SERVER sea el servidor de tomcat http://13.59.249.130:8080/

--- fidouafclient/app/src/main/java/org/ebayopensource/fidouafclient/util/Endpoints.java
+++ fidouafclient/app/src/main/java/org/ebayopensource/fidouafclient/util/Endpoints.java
@@ -3,7 +3,7 @@ package org.ebayopensource.fidouafclient.util;
 public class Endpoints {
 
 	public static final String SERVER =
-	"http://openidconnect.ebay.com";
+	"http://13.59.249.130:8080/";
 	//"http://www.head2toes.org";
 	public static final String GET_AUTH_REQUEST = "/fidouaf/v1/public/authRequest";
 	public static final String POST_AUTH_RESPONSE = "/fidouaf/v1/public/authResponse";
